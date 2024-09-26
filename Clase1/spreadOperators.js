5504903d6b526c1b180d3e75661f1d82eac21c09
DESKTOP-OE7RG1K\Usuario <chalave21@outlook.com>
2024-09-25 22:41:23 -0300

    spreadOperators

--- Clase1/spreadOperators.js
+++ Clase1/spreadOperators.js
@@ -1,4 +1,5 @@
-// se usa en las estructuras de datos como objetos y arrays, para copiar el contenido que tiene dentro
+// ...spreadOperator --> {...speadOperator}  --> [...spreadOperator]
+//se usa en las estructuras de datos como objetos y arrays, para copiar el contenido que tiene dentro
 // y exparsilo dentro de otro objeto o array. Porque sino se hace una copia por referencia y cualquiera de
 // Los dos valores que se modifiquen , repercute en el otro.
 
